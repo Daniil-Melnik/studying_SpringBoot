6e7fe7652541a1d4d1433b3bf97ef1208e136045
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-07 13:11:02 +0300

    added html template, added base controller, added DB_func

--- blog/src/main/java/com/example/blog/controllers/BlogController.java
+++ blog/src/main/java/com/example/blog/controllers/BlogController.java
@@ -4,6 +4,8 @@ package com.example.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.blog.Auto;
 import com.example.blog.DataBase;
@@ -18,5 +20,17 @@ public class BlogController {
 		Iterable<Auto> auto_list = DataBase.getAutos();
 		model.addAttribute("autos", auto_list);
 		return "blog-main";
+	}
+
+	@GetMapping("/blog/add")
+	public String blogAdd(Model model) {
+		return "blog-add";
+	}
+
+	@PostMapping("/blog/add")
+	public String blogPostAdd(@RequestParam String name, @RequestParam String comand, @RequestParam String full_text, Model model){
+		Auto auto = new Auto();
+		auto.setAuto(0, name, comand, full_text, "https://i.ytimg.com/vi/BnvuQe6KcU8/maxresdefault.jpg");
+		return "";
 	}
 }
